050e30fa4c64ccae958267ccb3205f4fd72de526
Jithin2k <jithinjmadhav@gmail.com>
2025-09-26 12:37:39 +0530

    Cloudinary Configuration

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDb from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 // App Config
 const app = express();
 const port = process.env.PORT || 4000;
 
 connectDb();
+connectCloudinary();
 
 // Middleware
 app.use(express.json());
