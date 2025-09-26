09994fb75cc9a299ea186321cf49c7c6b7a2b457
Jithin2k <jithinjmadhav@gmail.com>
2025-09-26 12:25:34 +0530

    MongoDB Configuration and Setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDb from "./config/mongodb.js";
 
 // App Config
 const app = express();
 const port = process.env.PORT || 4000;
+
+connectDb();
 
 // Middleware
 app.use(express.json());
