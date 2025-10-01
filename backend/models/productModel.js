94086e5f42075b5c25183b4f4db5c1a7430521e4
Jithin2k <jithinjmadhav@gmail.com>
2025-10-01 09:55:48 +0530

    Created Product Schema

---
+++ backend/models/productModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  bestseller: {
+    type: Boolean,
+    required: true,
+  },
+  date: { type: Number, required: true },
+});
+
+export default mongoose.model("Product",productSchema);
