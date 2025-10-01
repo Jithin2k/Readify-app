9a819dc1741bdce6028ce9d73f7d3acaed29064f
Jithin2k <jithinjmadhav@gmail.com>
2025-10-01 11:25:48 +0530

    Createf Login API

--- backend/controllers/userContoller.js
+++ backend/controllers/userContoller.js
@@ -3,7 +3,36 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(409)
+        .json({ success: false, message: "User does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+      res.status(200).json({ success: true, token });
+    } else {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid Password" });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong. Please try again later.",
+      error: error.message,
+    });
+  }
+};
 
 const registerUser = async (req, res) => {
   try {
