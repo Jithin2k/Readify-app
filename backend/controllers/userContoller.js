d7591bb77184bf057e4e23bc5785d60fee741057
Jithin2k <jithinjmadhav@gmail.com>
2025-10-01 10:54:15 +0530

    Created Register user API

---
+++ backend/controllers/userContoller.js
@@ -0,0 +1,58 @@
+import userModel from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const loginUser = async (req, res) => {};
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const exists = await userModel.findOne({ email });
+
+    if (exists) {
+      return res
+        .status(409)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Enter a valid email" });
+    }
+
+    if (!validator.isStrongPassword(password)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Enter a strong password" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const user = await newUser.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    res.status(200).json({ success: true, token });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong. Please try again later.",
+      error: error.message,
+    });
+  }
+};
+
+const adminLogin = async (req, res) => {};
+
+export { loginUser, registerUser, adminLogin };
