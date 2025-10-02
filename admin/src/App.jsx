31e14e9b019cfa2c8e6d56af74f07f6b1906937f
Jithin2k <jithinjmadhav@gmail.com>
2025-10-02 12:21:36 +0530

    Created Sidebar Component and setup routing

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./Pages/Add";
+import List from "./Pages/List";
+import Orders from "./Pages/Orders";
 
 const App = () => {
   return (
-    <>
-<Navbar/>
-    </>
-  )
-}
+    <div className="min-h-screen">
+      <>
+        <Navbar />
+        <hr />
+        <div className="flex w-full">
+          <Sidebar />
+          <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-base">
+            <Routes>
+              <Route path="/add" element={<Add />} />
+              <Route path="/list" element={<List />} />
+              <Route path="/orders" element={<Orders />} />
+            </Routes>
+          </div>
+        </div>
+      </>
+    </div>
+  );
+};
 
-export default App
+export default App;
