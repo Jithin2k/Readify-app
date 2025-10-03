f0c88d8f7f4f3769864dedbab66f6089b3d06d11
Jithin2k <jithinjmadhav@gmail.com>
2025-10-03 11:12:20 +0530

    Added toast notificationto ADD.jsx

--- admin/src/Pages/Add.jsx
+++ admin/src/Pages/Add.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { assets } from "../assets/assets";
 import axios from "axios";
 import { backendUrl } from "../App";
+import { toast } from "react-toastify";
 
 const Add = ({token}) => {
   const [image, setImage] = useState(false);
@@ -25,9 +26,22 @@ const Add = ({token}) => {
       image && formData.append("image",image); 
 
       const response = await axios.post(backendUrl + "/api/product/add",formData,{headers:{token}});
-      console.log(response);
+      
+      if(response.data.success){
+        toast.success(response.data.message);
+        setName("")
+        setAuthor("");
+        setDescription("");
+        setCategory("");
+        setPrice("");
+        setImage(false);
+      } else{
+        toast.error(response.data.message)
+      }
 
      } catch (error) {
+      console.log(error);
+      toast.error(error.message)
       
      }
   }
