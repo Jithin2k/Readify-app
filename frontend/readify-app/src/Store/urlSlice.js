3bd4f923e791f9ba230201772a2bc979616f742e
Jithin2k <jithinjmadhav@gmail.com>
2025-10-15 10:54:16 +0530

    Frontend url bug fix

--- frontend/readify-app/src/Store/urlSlice.js
+++ frontend/readify-app/src/Store/urlSlice.js
@@ -1,5 +1,5 @@
-// export const backendUrl = import.meta.env.VITE_BACKEND_URL;
-export const backendUrl= "http://localhost:4000";
+ export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+// export const backendUrl= "http://localhost:4000";
 
 import { createSlice } from "@reduxjs/toolkit";
 
